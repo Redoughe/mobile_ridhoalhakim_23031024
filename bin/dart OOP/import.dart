c6e033f213fef94bb374e406a0f373ae78e023db
Redoughe <ridhoalhakim.e@gmail.com>
2025-11-13 22:33:05 +0700

    4th Commit

---
+++ bin/dart OOP/import.dart
@@ -0,0 +1,8 @@
+import '/Ridho/Kuliah/P.Mobile/mobile_ridhoalhakim_23031024/bin/dart dasar/category.dart';
+
+void main(){
+  var category = Category("1", "Food");
+
+  print(category.id);
+  print(category.name);
+}
